7e4c0ddea9dd4fc10f0ed1b6aa8b0dddd25abcfc
aeneasr <3372410+aeneasr@users.noreply.github.com>
2022-01-05 15:30:06 +0100

    refactor: deprecate domain aliases
    BREAKING CHANGES: This patch removes the ability to use domain aliases, an obscure feature rarely used that had several issues and inconsistencies.

--- x/http_secure_redirect.go
+++ x/http_secure_redirect.go
@@ -134,7 +134,7 @@ func SecureContentNegotiationRedirection(
 			append([]SecureRedirectOption{
 				SecureRedirectUseSourceURL(requestURL),
 				SecureRedirectAllowURLs(c.SelfServiceBrowserWhitelistedReturnToDomains()),
-				SecureRedirectAllowSelfServiceURLs(c.SelfPublicURL(r)),
+				SecureRedirectAllowSelfServiceURLs(c.SelfPublicURL()),
 			}, opts...)...,
 		)
 		if err != nil {
